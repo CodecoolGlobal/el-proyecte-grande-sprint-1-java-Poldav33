5a101806e82fe01eab4035d7cf6cadbd6204ebeb
SilentJJ <testy@freemail.hu>
2023-12-06 14:57:00 +0100

    add user table to the DB

--- backend/src/main/resources/create_db.sql
+++ backend/src/main/resources/create_db.sql
@@ -1,6 +1,7 @@
 --- Drop Table
 
 DROP TABLE IF EXISTS calories;
+DROP TABLE IF EXISTS users;
 
 --- Create calories
 
@@ -10,6 +11,18 @@ CREATE TABLE calories (
     calories_per_hour INT
 );
 
+CREATE TABLE users(
+                      id SERIAL PRIMARY KEY,
+                      name TEXT NOT NULL,
+                      username TEXT NOT NULL UNIQUE,
+                      password TEXT NOT NULL ,
+                      email TEXT NOT NULL UNIQUE
+);
+
 INSERT INTO calories (name, calories_per_hour) VALUES ('water skiing', 354);
 INSERT INTO calories (name, calories_per_hour) VALUES ('Running', 835);
 INSERT INTO calories (name, calories_per_hour) VALUES ('Hiking', 435);
+
+INSERT INTO users (name, username, password, email)
+VALUES ('Geri', 'gerike', 'ekireg', 'geri@geri.geri'),
+       ('Dávid', 'polgi', 'iglop', 'polgi@iglop.polgi');
