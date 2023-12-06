e41891ff7e5dda14221752f03cf24fefccc52e80
SilentJJ <testy@freemail.hu>
2023-12-06 13:58:35 +0100

    add activity controller

---
+++ backend/src/main/java/com/codecool/backend/controller/ActivityController.java
@@ -0,0 +1,25 @@
+package com.codecool.backend.controller;
+
+import com.codecool.backend.dao.model.Activity;
+import com.codecool.backend.service.ActivityService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/activities")
+public class ActivityController {
+
+    private final ActivityService activityService;
+
+    public ActivityController(ActivityService activityService) {
+        this.activityService = activityService;
+    }
+
+    @GetMapping("/")
+    public List<Activity> getActivities() {
+        return activityService.getActivities();
+    }
+}
