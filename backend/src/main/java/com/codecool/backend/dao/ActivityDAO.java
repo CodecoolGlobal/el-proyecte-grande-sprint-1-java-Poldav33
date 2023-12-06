5b8c299f8715c55edf018365af8ef0e1c45fa80a
SilentJJ <testy@freemail.hu>
2023-12-06 13:57:51 +0100

    add activity DAO interface

---
+++ backend/src/main/java/com/codecool/backend/dao/ActivityDAO.java
@@ -0,0 +1,9 @@
+package com.codecool.backend.dao;
+
+import com.codecool.backend.dao.model.Activity;
+
+import java.util.List;
+
+public interface ActivityDAO {
+    List<Activity> getActivities();
+}
