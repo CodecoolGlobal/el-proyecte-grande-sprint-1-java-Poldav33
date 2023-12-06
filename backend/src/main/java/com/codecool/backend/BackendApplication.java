90fc5eccedda25c2b1fa98316fc1ac83152daccb
SilentJJ <testy@freemail.hu>
2023-12-06 13:58:55 +0100

    add activity service Bean

--- backend/src/main/java/com/codecool/backend/BackendApplication.java
+++ backend/src/main/java/com/codecool/backend/BackendApplication.java
@@ -1,13 +1,22 @@
 package com.codecool.backend;
 
+import com.codecool.backend.dao.ActivityDAO;
+import com.codecool.backend.dao.ActivityDAOJdbc;
+import com.codecool.backend.dao.connection.PSQLConnector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @Bean
+    public ActivityDAO activityDAO(PSQLConnector connector) {
+        return new ActivityDAOJdbc(connector);
     }
 
 }
