15387818c647baa320ddc5128643c51ddcd6089f
SilentJJ <testy@freemail.hu>
2023-12-07 20:40:04 +0100

    remove the homepage from the navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css'
 import Navigationbar from "./components/navigationbar";
-import HomePage from "./pages/HomePage.tsx";
 
 function App() {
 
   return (
       <div className={"App"}>
             <Navigationbar/>
-            <HomePage />
       </div>
   )
 }
