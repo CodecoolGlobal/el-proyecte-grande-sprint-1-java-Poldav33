2c25aad4c7a66f90cdca6f68d9991da4913507b9
Poldav33 <poldav33@gmail.com>
2024-01-02 15:29:31 +0100

    Rename & refactor Activities

---
+++ backend/src/main/java/com/codecool/backend/controller/ExerciseController.java
@@ -0,0 +1,26 @@
+package com.codecool.backend.controller;
+
+import com.codecool.backend.dao.model.Exercise;
+import com.codecool.backend.service.ExerciseService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/activities")
+public class ExerciseController {
+
+    private final ExerciseService exerciseService;
+
+    public ExerciseController(ExerciseService exerciseService) {
+        this.exerciseService = exerciseService;
+    }
+
+    @GetMapping("")
+    public List<Exercise> getActivities() {
+        return exerciseService.getActivities();
+    }
+
+}
