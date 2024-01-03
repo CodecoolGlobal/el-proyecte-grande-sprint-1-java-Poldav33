d6cd8028b3155eae13e1893d9ef8ae527f977cfa
Poldav33 <poldav33@gmail.com>
2024-01-03 11:23:10 +0100

    Add filter endpoint

--- backend/src/main/java/com/codecool/backend/controller/ExerciseController.java
+++ backend/src/main/java/com/codecool/backend/controller/ExerciseController.java
@@ -23,4 +23,8 @@ public class ExerciseController {
         return exerciseService.getActivities();
     }
 
+    @GetMapping("/filter")
+    public List<Exercise> getFilteredActivities(){
+        return null;
+    }
 }
