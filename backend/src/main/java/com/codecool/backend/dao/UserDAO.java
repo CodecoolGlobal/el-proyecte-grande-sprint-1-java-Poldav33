53a87e725b5de6cb809b28a3dcf4866fd86d36bf
Andi Gruber <109924492+gruberandi@users.noreply.github.com>
2024-01-03 14:50:43 +0100

    User Controller

--- backend/src/main/java/com/codecool/backend/dao/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.backend.dao;
-
-import com.codecool.backend.controller.dto.NewUserDTO;
-
-public interface UserDAO {
-    boolean addUser(NewUserDTO newUserDTO);
-
-}
--- frontend/src/components/userloginform/UserLoginForm.tsx
+++ frontend/src/components/userloginform/UserLoginForm.tsx
@@ -26,7 +26,7 @@ const UserLogin = ({loginUser} : loginUserProp) => {
             <input type={"text"} name={"name"} onChange={handleChange} value={user.name}/>
             <label htmlFor={"email"}>Email: </label>
             <input type={"text"} name={"email"} onChange={handleChange} value={user.email}/>
-            <label htmlFor={"password"}>Password: </label>
+            <label htmlFor={"password"}>Password: </label>git
             <input type={"text"} name={"password"} onChange={handleChange} value={user.password}/>
         </form>
     );
