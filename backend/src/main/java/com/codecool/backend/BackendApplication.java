c9174f75e49cf18c13dce3166332f1b35a8704ce
Andi Gruber <109924492+gruberandi@users.noreply.github.com>
2024-01-03 14:49:01 +0100

    BackendApplication modify

--- backend/src/main/java/com/codecool/backend/BackendApplication.java
+++ backend/src/main/java/com/codecool/backend/BackendApplication.java
@@ -1,10 +1,6 @@
 package com.codecool.backend;
 
-import com.codecool.backend.dao.ActivityDAO;
-import com.codecool.backend.dao.ActivityDAOJdbc;
-import com.codecool.backend.dao.UserDAO;
-import com.codecool.backend.dao.UserDAOJdbc;
-import com.codecool.backend.dao.connection.PSQLConnector;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,14 +12,6 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
-    @Bean
-    public ActivityDAO activityDAO(PSQLConnector connector) {
-        return new ActivityDAOJdbc(connector);
-    }
 
-    @Bean
-    public UserDAO userDAO(PSQLConnector connector) {
-        return new UserDAOJdbc(connector);
-    }
 
 }
