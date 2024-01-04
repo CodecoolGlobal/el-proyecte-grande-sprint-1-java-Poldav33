d8f80c872c82fe7238ad11dfbed5cee8295b3191
Poldav33 <poldav33@gmail.com>
2024-01-04 10:26:56 +0100

    delete methods, exerciseService will use exerciseRepositoryAll

--- backend/src/main/java/com/codecool/backend/service/ExerciseService.java
+++ backend/src/main/java/com/codecool/backend/service/ExerciseService.java
@@ -1,56 +1,24 @@
 package com.codecool.backend.service;
 
 import com.codecool.backend.controller.dto.FilterDTO;
-import com.codecool.backend.dao.ExerciseDAO;
-import com.codecool.backend.dao.model.Exercise;
+Delete import com.codecool.backend.model.Exercise;
 import com.codecool.backend.repository.ExerciseRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import org.hibernate.Session;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.jpa.provider.HibernateUtils;
+import com.codecool.backend.repository.ExerciseRepositoryAll;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ExerciseService {
+    private final ExerciseRepositoryAll exerciseRepositoryAll;
 
-    private final ExerciseDAO activityDAO;
-    private final ExerciseRepository exerciseRepository;
-    private final EntityManagerFactory emf;
-    @Value("${connectionstring}")
-    private String url;
-    @Value("${dbpassword}")
-    private String dbPassword;
-    @Value("${dbusername}")
-    private String dbUsername;
 
-    public ExerciseService(ExerciseDAO activityDAO, ExerciseRepository exerciseRepository, EntityManager em) {
-        this.activityDAO = activityDAO;
-        this.exerciseRepository = exerciseRepository;
-        this.emf = Persistence.createEntityManagerFactory(
-                url + ";" + "user=" + dbUsername + ";" + "password=" + dbPassword
-        );
+    public ExerciseService(ExerciseRepositoryAll exerciseRepositoryAll) {
+        this.exerciseRepositoryAll = exerciseRepositoryAll;
     }
 
-    public List<Exercise> getActivities() {
-        return activityDAO.getExercises();
-    }
-    public List<Exercise> getFilteredExercises(FilterDTO filterDTO) {
-//        EntityManager em = emf.createEntityManager();
-//        CriteriaBuilder cb = em.getCriteriaBuilder();
-//        CriteriaQuery<Exercise> query = cb.createQuery(Exercise.class);
-//        Root<Exercise> c = query.from(Exercise.class);
-
-        try (Session session = HibernateUtils) {
-
-        } catch(Exception e) {
-            System.out.println("asd");
-        }
-        Session session = HibernateUtils.getHibernateSession();
+    public List<Exercise> getFilterByDifficulty(FilterDTO filterDTO) {
+        return exerciseRepositoryAll.filterExercise(filterDTO);
     }
 }
