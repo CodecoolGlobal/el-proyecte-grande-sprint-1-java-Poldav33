13640c94165aecbc14f4e6b15e073a1c7f134f8c
SilentJJ <testy@freemail.hu>
2024-01-04 12:50:54 +0100

    add exercise filter endpoint

--- backend/src/main/java/com/codecool/backend/controller/ExerciseController.java
+++ backend/src/main/java/com/codecool/backend/controller/ExerciseController.java
@@ -1,26 +1,63 @@
 package com.codecool.backend.controller;
 
-import com.codecool.backend.dao.model.Exercise;
+import com.codecool.backend.controller.dto.FilterDTO;
+import com.codecool.backend.controller.dto.ExerciseDTO;
+import com.codecool.backend.model.Exercise;
 import com.codecool.backend.service.ExerciseService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/activities")
+@RequestMapping("/api/exercises")
 public class ExerciseController {
 
     private final ExerciseService exerciseService;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     public ExerciseController(ExerciseService exerciseService) {
         this.exerciseService = exerciseService;
     }
 
     @GetMapping("")
-    public List<Exercise> getActivities() {
-        return exerciseService.getActivities();
+    public List<ExerciseDTO> getActivities() {
+        List<ExerciseDTO> exerciseDTOS = new ArrayList<>();
+        List<Exercise> exercises = exerciseService.getActivities();
+        exercises.forEach(exercise -> {
+            exerciseDTOS.add(new ExerciseDTO(
+                    exercise.getId(),
+                    exercise.getName(),
+                    exercise.getType(),
+                    exercise.getMuscle(),
+                    exercise.getDifficulty()
+            ));
+        });
+        return exerciseDTOS;
+    }
+
+    @GetMapping("test")
+    public FilterDTO test() {
+        return new FilterDTO("key", "value");
+    }
+
+    @PostMapping("filter")
+    public ResponseEntity<List<Exercise>> getFilteredExercises(@RequestBody List<FilterDTO> filterDTOS) {
+        try {
+            return ResponseEntity.ok().body(exerciseService.getFilteredExercises(filterDTOS));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatusCode.valueOf(404));
+        }
+    }
+
+    private List<FilterDTO> createFilterDTOs() {
+        return null;
     }
 
 }
