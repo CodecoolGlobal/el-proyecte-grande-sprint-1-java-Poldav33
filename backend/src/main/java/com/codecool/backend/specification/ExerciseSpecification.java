417d7a5fff34631946799b9b81a71cca0d94a264
SilentJJ <testy@freemail.hu>
2024-01-04 12:52:36 +0100

    refactor ExerciseSpecification

--- backend/src/main/java/com/codecool/backend/specification/ExerciseSpecification.java
+++ backend/src/main/java/com/codecool/backend/specification/ExerciseSpecification.java
@@ -1,32 +1,26 @@
 package com.codecool.backend.specification;
 
 import com.codecool.backend.controller.dto.FilterDTO;
-import com.codecool.backend.dao.model.Exercise;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
+import com.codecool.backend.model.Exercise;
 import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ExersiceSpecification {
+public class ExerciseSpecification {
 
     public static Specification<Exercise> columnEqual(List<FilterDTO> filterDTOS) {
 
-        return new Specification<Exercise>() {
-            @Override
-            public Predicate toPredicate(Root<Exercise> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+        return (root, query, criteriaBuilder) -> {
 
-                List<Predicate> predicates = new ArrayList<>();
-                filterDTOS.forEach(filter -> {
-                    Predicate predicate = criteriaBuilder.equal(root.get(filter.columnName()), filter.columnValue());
-                    predicates.add(predicate);
-                });
+            List<Predicate> predicates = new ArrayList<>();
+            filterDTOS.forEach(filter -> {
+                Predicate predicate = criteriaBuilder.like(root.get(filter.columnName()), "%" + filter.columnValue() + "%");
+                predicates.add(predicate);
+            });
 
-                return criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
-            }
+            return criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
         };
     }
 }
