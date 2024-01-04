fa38c1ca6c039a85e649e3ba673847df1f113249
Poldav33 <poldav33@gmail.com>
2024-01-04 10:28:04 +0100

    Add basic find methods

--- backend/src/main/java/com/codecool/backend/repository/ExerciseRepository.java
+++ backend/src/main/java/com/codecool/backend/repository/ExerciseRepository.java
@@ -1,10 +1,15 @@
 package com.codecool.backend.repository;
 
-import com.codecool.backend.dao.model.Exercise;
+import com.codecool.backend.model.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
-    Optional<Exercise> findById(long id);
+public interface ExerciseRepository extends JpaRepository<Exercise, Long>{
+    Optional<Exercise> findByName(String name);
+    Optional<Exercise> findByType(String type);
+    Optional<Exercise> findByDifficulty(String difficulty);
+    Optional<Exercise> findByMuscle(String muscle);
+
+
 }
