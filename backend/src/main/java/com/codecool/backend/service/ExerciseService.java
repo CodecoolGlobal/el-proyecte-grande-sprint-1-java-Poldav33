8b4406fea8ef19d7e8d70d4d7700ab3150887ed0
SilentJJ <testy@freemail.hu>
2024-01-04 12:52:12 +0100

    refactor ExerciseService and add getFilteredExercise method

--- backend/src/main/java/com/codecool/backend/service/ExerciseService.java
+++ backend/src/main/java/com/codecool/backend/service/ExerciseService.java
@@ -1,24 +1,27 @@
 package com.codecool.backend.service;
 
-import com.codecool.backend.dao.ExerciseDAO;
-import com.codecool.backend.dao.model.Exercise;
+import com.codecool.backend.controller.dto.FilterDTO;
+import com.codecool.backend.controller.dto.ExerciseDTO;
+import com.codecool.backend.model.Exercise;
 import com.codecool.backend.repository.ExerciseRepository;
+import com.codecool.backend.specification.ExerciseSpecification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ExerciseService {
-
-    private final ExerciseDAO activityDAO;
     private final ExerciseRepository exerciseRepository;
 
-    public ExerciseService(ExerciseDAO activityDAO, ExerciseRepository exerciseRepository) {
-        this.activityDAO = activityDAO;
+    public ExerciseService(ExerciseRepository exerciseRepository) {
         this.exerciseRepository = exerciseRepository;
     }
 
     public List<Exercise> getActivities() {
-        return activityDAO.getExercises();
+        return exerciseRepository.findAll();
+    }
+
+    public List<Exercise> getFilteredExercises(List<FilterDTO> filterDTOS) {
+        return exerciseRepository.findAll(ExerciseSpecification.columnEqual(filterDTOS));
     }
 }
