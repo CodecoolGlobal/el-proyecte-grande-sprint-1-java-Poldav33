3e19f00982254b2dd3cb1b0bd6bec1d8ff21bd70
SilentJJ <testy@freemail.hu>
2024-01-04 12:50:21 +0100

    add exercise entity table name

--- backend/src/main/java/com/codecool/backend/model/Exercise.java
+++ backend/src/main/java/com/codecool/backend/model/Exercise.java
@@ -3,8 +3,10 @@ package com.codecool.backend.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="exercises")
 public class Exercise {
     @Id
     private long id;
