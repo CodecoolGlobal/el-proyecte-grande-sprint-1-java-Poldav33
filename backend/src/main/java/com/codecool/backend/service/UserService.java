82904720549e8c42f4f9e08fdb353f31ad7a5698
Andi Gruber <109924492+gruberandi@users.noreply.github.com>
2024-01-04 09:20:21 +0100

    User Controller

---
+++ backend/src/main/java/com/codecool/backend/service/UserService.java
@@ -0,0 +1,10 @@
+package com.codecool.backend.service;
+
+import com.codecool.backend.controller.dto.NewUserDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+}
