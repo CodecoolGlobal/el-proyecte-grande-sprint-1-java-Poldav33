c159340e1654b380e1999c062ef32cc0639fada8
Poldav33 <poldav33@gmail.com>
2024-01-04 10:28:49 +0100

    change endpoint and implement filter endpoint

--- backend/src/main/java/com/codecool/backend/controller/ExerciseController.java
+++ backend/src/main/java/com/codecool/backend/controller/ExerciseController.java
@@ -1,15 +1,14 @@
 package com.codecool.backend.controller;
 
-import com.codecool.backend.dao.model.Exercise;
+import com.codecool.backend.controller.dto.FilterDTO;
+import com.codecool.backend.model.Exercise;
 import com.codecool.backend.service.ExerciseService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/activities")
+@RequestMapping("/api/exercises")
 public class ExerciseController {
 
     private final ExerciseService exerciseService;
@@ -18,13 +17,14 @@ public class ExerciseController {
         this.exerciseService = exerciseService;
     }
 
-    @GetMapping("")
-    public List<Exercise> getActivities() {
-        return exerciseService.getActivities();
+//    @GetMapping("")
+//    public List<Exercise> getExercises() {
+//        return exerciseService.getActivities();
+//    }
+
+    @PostMapping("/filter")
+    public List<Exercise> getFilteredExercises(@RequestBody FilterDTO filterDTO){
+        return exerciseService.getFilterByDifficulty(filterDTO);
     }
 
-    @GetMapping("/filter")
-    public List<Exercise> getFilteredActivities(){
-        return null;
-    }
 }
