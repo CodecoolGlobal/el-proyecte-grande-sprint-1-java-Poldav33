6c61ff4f7efcc437ccf2b50331f0ca7da75aafbd
Farkas Gergo <128323984+Fgergo1@users.noreply.github.com>
2024-01-15 15:38:20 +0100

    nutrition page/form is added

--- frontend/src/type/NutritionType.ts
+++
@@ -1,9 +0,0 @@
-interface NutritionType {
-    name: string,
-    calories: number,
-    fat_total_g: number,
-    carbohydrates_total_g: number,
-    fiber_g: number
-}
-
-export default NutritionType;
