5d9fe0f3ba8290d81950a6b41a37aedcb75f3c83
SilentJJ <testy@freemail.hu>
2024-01-15 15:00:10 +0100

    refactor exercise filter endpoint

--- backend/src/main/java/com/codecool/backend/controller/ExerciseController.java
+++ backend/src/main/java/com/codecool/backend/controller/ExerciseController.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
