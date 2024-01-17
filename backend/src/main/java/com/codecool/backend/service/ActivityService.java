6b209ba8bab87f107194714268870316b9aadde0
Poldav33 <poldav33@gmail.com>
2024-01-17 14:40:14 +0100

    Create ActivityService with save, addTraining, findby methods

---
+++ backend/src/main/java/com/codecool/backend/service/ActivityService.java
@@ -0,0 +1,67 @@
+package com.codecool.backend.service;
+
+import com.codecool.backend.controller.dto.ActivityDTO;
+import com.codecool.backend.controller.dto.NewActivityDTO;
+import com.codecool.backend.controller.mapper.ActivityMapper;
+import com.codecool.backend.model.Activity;
+import com.codecool.backend.model.Training;
+import com.codecool.backend.model.User;
+import com.codecool.backend.repository.ActivityRepository;
+import com.codecool.backend.repository.TrainingRepository;
+import com.codecool.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class ActivityService {
+    private ActivityRepository activityRepository;
+    private UserRepository userRepository;
+    private TrainingRepository trainingRepository;
+    @Autowired
+    public ActivityService(ActivityRepository activityRepository, UserRepository userRepository, TrainingRepository trainingRepository) {
+        this.activityRepository = activityRepository;
+        this.userRepository = userRepository;
+        this.trainingRepository = trainingRepository;
+    }
+
+    public Optional<Activity> findActivityById(Long id) {
+        return activityRepository.findById(id);
+    }
+    public void saveActivity(NewActivityDTO newActivityDTO){
+        Optional<User> userOptional = userRepository.findByUserId(newActivityDTO.userId());
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            activityRepository.save(new Activity(
+                    user,
+                    newActivityDTO.date(),
+                    newActivityDTO.description()
+            ));
+        }
+    }
+    public Activity getActivityById(Long id) {
+        Optional<Activity> optionalActivity = activityRepository.findByActivityId(id);
+        if (optionalActivity.isPresent()) {
+            Activity activity = optionalActivity.get();
+            return activity;
+        }
+        return null;
+    }
+    public void addTraining(Long trainingId, Long activityId) {
+        Optional<Training> trainingOptional = trainingRepository.findByTrainingId(trainingId);
+        Optional<Activity> activityOptional = activityRepository.findByActivityId(activityId);
+        if (trainingOptional.isPresent() && activityOptional.isPresent()) {
+
+            Training training = trainingOptional.get();
+            Activity activity = activityOptional.get();
+
+            activity.addTraining(training);
+            training.setActivity(activity);
+
+            trainingRepository.save(training);
+            activityRepository.save(activity);
+        }
+    }
+}
