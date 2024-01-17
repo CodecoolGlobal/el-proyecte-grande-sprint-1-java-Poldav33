5585ae39ba65e7cfdf647f82159806f859d00d31
Poldav33 <poldav33@gmail.com>
2024-01-17 14:38:10 +0100

    Create TrainingRepository

---
+++ backend/src/main/java/com/codecool/backend/repository/TrainingRepository.java
@@ -0,0 +1,11 @@
+package com.codecool.backend.repository;
+
+import com.codecool.backend.model.Training;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.Set;
+
+public interface TrainingRepository extends JpaRepository<Training, Long> {
+    Optional<Training> findByTrainingId(Long id);
+}
