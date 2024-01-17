8fb399f87ce8aa17e92a4afe183dc5e2221ef0c4
Poldav33 <poldav33@gmail.com>
2024-01-17 14:38:30 +0100

    Create TrainingService with savetraining method

---
+++ backend/src/main/java/com/codecool/backend/service/TrainingService.java
@@ -0,0 +1,38 @@
+package com.codecool.backend.service;
+
+import com.codecool.backend.controller.dto.TrainingDTO;
+import com.codecool.backend.model.Activity;
+import com.codecool.backend.model.Exercise;
+import com.codecool.backend.model.Training;
+import com.codecool.backend.repository.ActivityRepository;
+import com.codecool.backend.repository.ExerciseRepository;
+import com.codecool.backend.repository.TrainingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class TrainingService {
+    private TrainingRepository trainingRepository;
+    private ActivityRepository activityRepository;
+    private ExerciseRepository exerciseRepository;
+    @Autowired
+    public TrainingService(TrainingRepository trainingRepository, ActivityRepository activityRepository, ExerciseRepository exerciseRepository) {
+        this.trainingRepository = trainingRepository;
+        this.activityRepository = activityRepository;
+        this.exerciseRepository = exerciseRepository;
+    }
+    public void saveTraining(TrainingDTO trainingDTO) {
+        Optional<Exercise> optionalExercise = exerciseRepository.
+                                                    findByExerciseId(trainingDTO.exerciseId());
+        optionalExercise.ifPresent(exercise -> trainingRepository
+                .save(new Training(
+                        exercise,
+                        trainingDTO.repeats(),
+                        trainingDTO.amount(),
+                        trainingDTO.durations())
+                ));
+    }
+}
