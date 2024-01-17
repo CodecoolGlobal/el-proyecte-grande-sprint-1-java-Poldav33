af1d3d40ccbdbba20760e704f960c18ca8530744
Poldav33 <poldav33@gmail.com>
2024-01-17 15:02:11 +0100

    Correct fieldName

--- backend/src/main/java/com/codecool/backend/repository/UserRepository.java
+++ backend/src/main/java/com/codecool/backend/repository/UserRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long> {
 
     Optional<User> findByEmailAndPassword(String email, String password);
+    Optional<User> findById(Long id);
 
 }
