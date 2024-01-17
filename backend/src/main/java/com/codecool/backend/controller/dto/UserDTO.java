145f10e74e1fe67041358449b20bed2d3c2ae3fa
Farkas Gergo <128323984+Fgergo1@users.noreply.github.com>
2024-01-17 10:21:53 +0100

    Modify entities and DTOs for security

--- backend/src/main/java/com/codecool/backend/controller/dto/UserDTO.java
+++ backend/src/main/java/com/codecool/backend/controller/dto/UserDTO.java
@@ -1,5 +1,5 @@
 package com.codecool.backend.controller.dto;
 
-public record UserDTO (String email, String password) {
+public record UserDTO (String username, String password) {
 
 }
--- backend/src/main/java/com/codecool/backend/model/UserEntity.java
+++ backend/src/main/java/com/codecool/backend/model/UserEntity.java
@@ -11,14 +11,13 @@ public class UserEntity {
     @Id
     @GeneratedValue
     private Long id;
-    private String name;
+
     private String username;
     private String password;
     private String email;
     private Role role;
 
-    public UserEntity(String name, String username, String password, String email, Role role) {
-        this.name = name;
+    public UserEntity(String username, String password, String email, Role role) {
         this.username = username;
         this.password = password;
         this.email = email;
@@ -32,16 +31,14 @@ public class UserEntity {
     public UserEntity() {
     }
 
-    public String getName() {
-        return name;
-    }
+
 
     public Long getId() {
         return id;
     }
 
     public String getUsername() {
-        return email;
+        return username;
     }
 
     public String getPassword() {
