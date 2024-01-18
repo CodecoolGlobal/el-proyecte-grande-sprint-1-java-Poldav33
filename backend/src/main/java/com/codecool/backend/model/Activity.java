70cb19d7cc56898c68f1408f95a18e89500f1fc3
SilentJJ <testy@freemail.hu>
2024-01-18 10:50:16 +0100

    Replace the User class with UserEntity

--- backend/src/main/java/com/codecool/backend/model/Activity.java
+++ backend/src/main/java/com/codecool/backend/model/Activity.java
@@ -1,11 +1,6 @@
 package com.codecool.backend.model;
 
-import com.codecool.backend.controller.dto.TrainingDTO;
-import com.codecool.backend.service.TrainingService;
 import jakarta.persistence.*;
-
-import java.time.LocalDate;
-import java.util.Optional;
 import java.util.Set;
 
 @Entity
@@ -15,7 +10,7 @@ public class Activity {
     private long id;
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
-    private User user;
+    private UserEntity user;
     private String date;
     private String description;
     @OneToMany(mappedBy = "activity")
@@ -23,7 +18,7 @@ public class Activity {
     public Activity() {
     }
 
-    public Activity(User user, String date, String description) {
+    public Activity(UserEntity user, String date, String description) {
         this.user = user;
         this.date = date;
         this.description = description;
@@ -33,7 +28,7 @@ public class Activity {
         return id;
     }
 
-    public User getUser() {
+    public UserEntity getUser() {
         return user;
     }
 
