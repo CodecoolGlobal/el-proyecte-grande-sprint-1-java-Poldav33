8d1df817ccec4fc9393a602dcf96b2be89bc908a
Poldav33 <poldav33@gmail.com>
2024-01-19 02:22:54 +0100

    Save new Activity with trainings

--- backend/src/main/java/com/codecool/backend/service/ActivityService.java
+++ backend/src/main/java/com/codecool/backend/service/ActivityService.java
@@ -1,41 +1,58 @@
 package com.codecool.backend.service;
 
-import com.codecool.backend.controller.dto.ActivityDTO;
 import com.codecool.backend.controller.dto.NewActivityDTO;
-import com.codecool.backend.controller.mapper.ActivityMapper;
+import com.codecool.backend.controller.dto.TrainingDTO;
 import com.codecool.backend.model.Activity;
 import com.codecool.backend.model.Training;
-import com.codecool.backend.model.User;
+import com.codecool.backend.model.UserEntity;
 import com.codecool.backend.repository.ActivityRepository;
+import com.codecool.backend.repository.ExerciseRepository;
 import com.codecool.backend.repository.TrainingRepository;
 import com.codecool.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-import java.util.Set;
 
 @Service
 public class ActivityService {
     private ActivityRepository activityRepository;
     private UserRepository userRepository;
     private TrainingRepository trainingRepository;
+    private ExerciseRepository exerciseRepository;
     @Autowired
-    public ActivityService(ActivityRepository activityRepository, UserRepository userRepository, TrainingRepository trainingRepository) {
+    public ActivityService(ActivityRepository activityRepository, UserRepository userRepository, TrainingRepository trainingRepository, ExerciseRepository exerciseRepository) {
         this.activityRepository = activityRepository;
         this.userRepository = userRepository;
         this.trainingRepository = trainingRepository;
+        this.exerciseRepository = exerciseRepository;
     }
-    public void saveActivity(NewActivityDTO newActivityDTO){
-        Optional<User> userOptional = userRepository.findById(newActivityDTO.userId());
+    public Activity saveActivity(NewActivityDTO newActivityDTO){
+        Optional<UserEntity> userOptional = userRepository.findById(newActivityDTO.userId());
         if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            activityRepository.save(new Activity(
+            UserEntity user = userOptional.get();
+            Activity newActivity = new Activity(
                     user,
                     newActivityDTO.date(),
                     newActivityDTO.description()
-            ));
+            );
+            activityRepository.save(newActivity);
+            System.out.println(newActivityDTO.trainingsDTO().length);
+            for (TrainingDTO trainingDTO : newActivityDTO.trainingsDTO()) {
+                System.out.println(trainingDTO.exerciseName());
+                System.out.println(exerciseRepository.findByName(trainingDTO.exerciseName()).get());
+                Training training = new Training(
+                        exerciseRepository.findByName(trainingDTO.exerciseName()).get(),
+                        trainingDTO.repeats(),
+                        trainingDTO.amount(),
+                        trainingDTO.durations(),
+                        newActivity
+                );
+                trainingRepository.save(training);
+            }
+            return newActivity;
         }
+        return null;
     }
     public Activity getActivityById(Long id) {
         Optional<Activity> optionalActivity = activityRepository.findById(id);
