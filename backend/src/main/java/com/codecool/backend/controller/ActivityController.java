c59771f04bd356f91496918b947bcac092f4886a
Poldav33 <poldav33@gmail.com>
2024-02-02 12:04:24 +0100

    remove unused imports

--- backend/src/main/java/com/codecool/backend/controller/ActivityController.java
+++ backend/src/main/java/com/codecool/backend/controller/ActivityController.java
@@ -1,10 +1,7 @@
 package com.codecool.backend.controller;
 
-import com.codecool.backend.controller.dto.ActivityDTO;
 import com.codecool.backend.controller.dto.NewActivityDTO;
-import com.codecool.backend.controller.dto.TrainingDTO;
 import com.codecool.backend.model.Activity;
-import com.codecool.backend.model.Training;
 import com.codecool.backend.service.ActivityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/api/activities")
