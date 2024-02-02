7471d44a41692b9fa158c84084dc4f155c5f9673
Poldav33 <poldav33@gmail.com>
2024-02-02 15:24:32 +0100

    Remove interface implementation and import them from type.ts

--- frontend/src/components/trainingdescription/TrainingDescription.tsx
+++ frontend/src/components/trainingdescription/TrainingDescription.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import {TrainingDescriptionProp} from "../../type/types.ts";
 
-const TrainingDescription = ({ description }) => {
+const TrainingDescription = ({ description }: TrainingDescriptionProp) => {
     return (
         <Paper elevation={3} style={{ padding: 16, marginTop: 16 }}>
             <Typography variant="h6" gutterBottom>
